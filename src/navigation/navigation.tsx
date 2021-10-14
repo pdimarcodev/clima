837fd0af789154c8a9077a1d2bc9da6195cda82d
pdimarcodev <pablo.d.dimarco@gmail.com>
2021-10-14 14:22:26 -0300

    first commit

---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens';
+
+const Stack = createStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
